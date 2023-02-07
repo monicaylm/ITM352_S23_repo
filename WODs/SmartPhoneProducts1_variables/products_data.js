e94df59f9e1e3b265364993e2cba217dba64b2a5
Monica Mau <122567612+monicaylm@users.noreply.github.com>
2023-02-07 09:07:54 -1000

    wod practice

---
+++ WODs/SmartPhoneProducts1_variables/products_data.js
@@ -0,0 +1,21 @@
+console.log("Product variables loaded...");
+
+        var name1 = "HTC";
+        var price1 = 40.00;
+        var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+   
+        var name2 = "Apple";
+        var price2 = 75.00;
+        var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+   
+        var name3 = "Nokia";
+        var price3 = 35.00;
+        var image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+   
+        var name4 = "Samsung";
+        var price4 = 45.00;
+        var image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+   
+        var name5 = "Blackberry";
+        var price5 = 10.00;
+        var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
