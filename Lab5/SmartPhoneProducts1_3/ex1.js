78de5e9aea9c389560d7485f3d0615b047217e74
monicaylm <mylm@hawaii.edu>
2023-02-07 09:19:30 -1000

    smartphoneproducts with variables

---
+++ Lab5/SmartPhoneProducts1_3/ex1.js
@@ -0,0 +1,9 @@
+age =  19
+fav_num = 16
+day_of_birth = 16
+month_of_birth = 5;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log(
+    (age + fav_num) / day_of_birth * month_of_birth)
