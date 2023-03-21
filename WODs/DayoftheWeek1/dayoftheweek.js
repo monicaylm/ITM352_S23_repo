e908732d5e9f045958a483ccf62534ac8830bc97
monicaylm <mylm@hawaii.edu>
2023-03-21 00:55:06 -1000

    wodss

--- WODs/DayoftheWeek1/dayoftheweek.js
+++ WODs/DayoftheWeek1/dayoftheweek.js
@@ -1,34 +1,33 @@
-var day = 16;
-var month = "April";
-var year = 2003;
+var day = 21;
+var month = "March";
+var year = 2023;
 
 if(month == 'January'||month == 'February'){
-    step1 = year - 1;
-} else {step1 = year};
+    x = year - 1;
+} else {x = year}
 
-step2 = parseInt(step1/4) + step1;
-step3 = step2-(parseInt(step1/100));
-step4 = parseInt(step1/400) + step3;
-step5 = step4 + day;
- 
+x_div4 = parseInt(x/4) + x;
+x_div100 = x_div4 - parseInt(x/100);
+x_div400 = parseInt(x/400) + x_div100;
+add_day = x_div400 + day;
+
 monthKey = {
-January: 0,
-February: 3,
-March: 2,
-April: 5,
-May: 0,
-June: 3,
-July: 5,
-August: 1,
-September: 4,
-October: 6,
-November: 2,
-December: 4
-};
+    January: 0,
+    February: 3,
+    March: 2,
+    April: 5,
+    May: 0,
+    June: 3,
+    July: 5,
+    August: 1,
+    September: 4,
+    October: 6,
+    November: 2,
+    December: 4
+}
 
-step6 = monthKey[month] + step5;
-step7 = step6%7
-
+add_monthKey = monthKey[month] + add_day;
+x_div7 = add_monthKey%7;
 
 dayNumber = [
     'Sunday',
@@ -38,9 +37,8 @@ dayNumber = [
     'Thursday',
     'Friday',
     'Saturday'
-]
+];
 
-
-dayoftheweek = dayNumber[step7]
+dayoftheweek = dayNumber[x_div7]
 
 console.log(month,day,year,dayoftheweek)
