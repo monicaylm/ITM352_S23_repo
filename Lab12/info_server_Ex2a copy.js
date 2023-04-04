ddf43ec0971344f5cf91f74772c533e8b9227c84
monicaylm <mylm@hawaii.edu>
2023-04-04 12:43:48 -1000

    lab12

--- Lab12/info_server_Ex2a copy.js
+++ Lab12/info_server_Ex2a copy.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path + ' with qs ' + JSON.stringify(request.query));
+    response.send(request.method + ' to path ' + request.path + ' with qs ' + JSON.stringify(request.query));
     next();
 });
+
+app.get('/test', function (request, response, next){
+    response.send('GET to test');;
+});
+
+
 app.use(express.static(__dirname + '/public')); 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
